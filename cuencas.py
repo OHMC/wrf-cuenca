cec7c820546b68fd4318e6756d9e05a253da0a64
Gonzalo Zigaran <gonzalo.zigaran@alumnos.unc.edu.ar>
2018-11-28 17:44:19 -0300

    Initial commit

---
+++ cuencas.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+import os
+import errno
+import glob
+import datetime
+from optparse import OptionParser
+
+
+import xarray as xr
+import numpy as np
+import pangaea as pa
+import pandas as pd
+import geopandas as gpd 
+import rasterio
+from rasterstats import zonal_stats
+import matplotlib.pyplot as plt
+
+def corregir_wrfout(ruta_wrfout):
+    xds =  xr.open_dataset(ruta_wrfout)
+    variables = ['XLAT', 'XLONG', 'XLAT_U', 'XLONG_U', 'XLAT_V', 'XLONG_V']
+    for var in variables:
+        if len(xds.coords['XLAT'].shape) > 2:
+            xds.coords[var] = xds.coords[var].mean(axis=0)
+    xds.to_netcdf(ruta_wrfout + '.nc')
+    rundate = datetime.datetime.strptime(xds.START_DATE, '%Y-%m-%d_%H:%M:%S')
+    xds.close()
+    return rundate
+
+    
+def abrir_plsm(ruta_wrfout):
+    plsm = pa.open_mfdataset(ruta_wrfout,
+                       lat_var='XLAT',
+                       lon_var='XLONG',
+                       time_var='XTIME',
+                       lat_dim='south_north',
+                       lon_dim='west_east',
+                       time_dim='Time')
+    return plsm
+
+
+def to_projection(plsm, variable, projection=None):
+    """Convert Grid to New Projection.
+        Parameters
+        ----------
+        variable: :obj:`str`
+            Name of variable in dataset.
+        projection: :func:`osr.SpatialReference`
+            Projection to convert data to.
+        Returns
+        -------
+        :func:`xarray.Dataset`
+    """
+    if projection == None:
+        from osgeo import osr, gdalconst
+        projection = osr.SpatialReference()
+        projection.ImportFromProj4("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    new_data = []
+    from gazar.grid import ArrayGrid
+    for band in range(plsm._obj.dims[plsm.time_dim]):
+        arr_grid = ArrayGrid(in_array=plsm._obj[variable][band].values[::-1,:],
+                             wkt_projection=plsm.projection.ExportToWkt(),
+                             geotransform=plsm.geotransform)
+        ggrid = arr_grid.to_projection(projection, gdalconst.GRA_Average)
+        new_data.append(ggrid.np_array())
+
+    plsm.to_datetime()
+    return plsm._export_dataset(variable, np.array(new_data),
+ggrid)
+
+
+def guardar_tif(vari, arr, out_path):
+    import rasterio
+    new_dataset = rasterio.open(out_path, 'w', driver='GTiff',
+                                height = arr.shape[0], width = arr.shape[1],
+                                count=1, dtype=str(arr.dtype),
+                                crs=vari.lsm.projection.ExportToWkt(),
+                                transform=vari.lsm.affine)
+    new_dataset.write(arr, 1)
+    new_dataset.close()
+
+    
+def convertir_variable(plsm, variable):
+    vari = to_projection(plsm.lsm, variable)
+    vari['lat'] = vari['lat'].sel(x=1)
+    vari['lon'] = vari['lon'].sel(y=1)
+    vari = vari.rename({'lat': 'y', 'lon': 'x'})
+    return vari
+    
+
+def genear_tif(ruta_wrfout, variable, time_idx, out_path):
+    plsm = abrir_plsm(ruta_wrfout)
+    vari = convertir_variable(plsm, variable)
+    arr = vari[variable][time_idx].values[:,:]
+    arr[arr == 0] = np.nan 
+    guardar_tif(vari, arr, out_path)
+    plsm.close()
+
+
+def genear_tif_prec(ruta_wrfout, out_path=None):
+    plsm = abrir_plsm(ruta_wrfout)
+    if out_path == None:
+        out_path = 'ppn_' + plsm.START_DATE[:-6] + '.tif'
+    plsm.variables['RAINNC'].values = plsm.variables['RAINNC'].values + 1000
+    plsm.variables['RAINC'].values = plsm.variables['RAINC'].values + 1000
+    rainnc = convertir_variable(plsm, 'RAINNC')
+    rainc = convertir_variable(plsm, 'RAINC')
+    arr_33 = rainnc.RAINNC[33].values[:,:] + rainc.RAINC[33].values[:,:]
+    arr_9 = rainnc.RAINNC[9].values[:,:] + rainc.RAINC[9].values[:,:]
+    arr = arr_33 - arr_9
+    arr[arr_9 == 0] = np.nan 
+    guardar_tif(rainnc, arr, out_path)
+    plsm.close()
+
+
+def integrar_en_cuencas(cuencas_gdf):
+    with rasterio.open("geotiff/ppn.tif") as src:
+        affine = src.transform
+        array = src.read(1)
+        df_zonal_stats = pd.DataFrame(zonal_stats(cuencas_gdf, array, affine=affine))
+    
+    cuencas_gdf_ppn = pd.concat([cuencas_gdf, df_zonal_stats], axis=1) 
+    cuencas_gdf_ppn = cuencas_gdf_ppn.dropna(subset=['mean'])
+    cuencas_gdf_ppn = cuencas_gdf_ppn.rename(columns={'Subcuenca' : 'subcuenca', 'Cuenca' : 'cuenca'})
+    return cuencas_gdf_ppn[['subcuenca', 'cuenca', 'geometry', 'count', 'max', 'mean', 'min']]
+
+
+def generar_imagen(cuencas_gdf_ppn, outdir, rundate, configuracion):
+    path = outdir + rundate.strftime('%Y_%m/%d/') + 'cuencas_' + configuracion + '.png'
+    try:
+        os.makedirs(os.path.dirname(path))
+    except OSError:
+        pass
+    f, ax = plt.subplots(1, figsize=(8, 8), frameon=False)
+    cuencas_gdf_ppn.dropna(subset=['mean']).plot(column='mean',
+                                                 scheme='fisher_jenks', 
+                                                 k=8,
+                                                 cmap=plt.cm.Blues,
+                                                 legend=True,
+                                                 ax=ax)
+    ax.set_axis_off()
+    plt.axis('equal')
+    plt.savefig(path, bbox_inches='tight')
+
+
+def guardar_tabla(cuencas_gdf_ppn, outdir, rundate, configuracion):
+    path = outdir + rundate.strftime('%Y_%m/%d/cordoba/') + 'cuencas_' + configuracion + '.csv'
+    try:
+        os.makedirs(os.path.dirname(path))
+    except OSError:
+        pass
+    cuencas_gdf_ppn = cuencas_gdf_ppn[['subcuenca', 'cuenca', 'count', 'max', 'mean', 'min']]
+    cuencas_gdf_ppn = cuencas_gdf_ppn.round(2)
+    cuencas_gdf_ppn.to_csv(path, index=False)
+
+
+def generar_producto_cuencas(wrfout, outdir_productos, outdir_tabla, configuracion):
+    # Abrimos DataFrame con las cuentas
+    cuencas_gdf = gpd.read_file('shapefiles/Cuencas hidrográficas.shp')
+
+    rundate = corregir_wrfout(wrfout)
+
+    genear_tif_prec(wrfout + '.nc', out_path='geotiff/ppn.tif')
+
+    cuencas_gdf_ppn = integrar_en_cuencas(cuencas_gdf)
+
+    generar_imagen(cuencas_gdf_ppn, outdir_productos, rundate, configuracion)
+
+    guardar_tabla(cuencas_gdf_ppn, outdir_tabla, rundate, configuracion)
+
+    # Eliminamos el wrfout que creamos
+    os.remove(wrfout + '.nc')
+
+
+
+
+def main():
+	usage = """cuencas.py [--wrfout=pathToDatosWRF] [--outdir_productos=..] [--outdir_tabla ..] [--configuracion ..]"""
+	parser = OptionParser(usage)
+
+	parser.add_option("--wrfout",dest="wrfout",help="ruta al wrfout de la salida del WRF")
+	parser.add_option("--outdir_productos",dest="outdir_productos",help="ruta donde se guardan los productos")
+	parser.add_option("--outdir_tabla",dest="outdir_tabla",help="ruta donde se guardan las tablas de datos")
+	parser.add_option("--configuracion",dest="configuracion",help="configuracion de las parametrizaciones")
+
+
+	(opts,args) = parser.parse_args()
+	if not opts.wrfout or not opts.outdir_tabla or not opts.outdir_productos or not opts.configuracion:
+		print("Faltan parametros!")
+		print(usage)
+	else:
+		wrfout = opts.wrfout
+		outdir_tabla = opts.outdir_tabla
+		outdir_productos = opts.outdir_productos
+		configuracion = opts.configuracion
+		generar_producto_cuencas(wrfout, outdir_productos, outdir_tabla, configuracion)
+
+if __name__ == "__main__":
+	main()
