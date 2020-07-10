2f7a059347e871112509da3008d754674087682a
agustinmontero <agustinmontero87@gmail.com>
2020-07-10 13:04:49 -0300

    Agregada excepcion y raise a la API

--- src/wrf_api/cuencas_api_ingest.py
+++ src/wrf_api/cuencas_api_ingest.py
@@ -1,12 +1,13 @@
 import datetime
 import logging
 import time
+from json.decoder import JSONDecodeError
 
 import requests
 from requests.exceptions import RequestException
 
 from config.logging_conf import INGESTOR_LOGGER_NAME
-from config.wrf_api_constants import API_BASE_URL_DICT, API_RESPONSES
+from config.wrf_api_constants import API_BASE_URL_DICT
 
 logger = logging.getLogger(INGESTOR_LOGGER_NAME)
 
@@ -14,12 +15,13 @@ logger = logging.getLogger(INGESTOR_LOGGER_NAME)
 def get_wrf_api_object_id(api_base_url, nombre, valor, campo):
     try:
         r = requests.get(f"{api_base_url}/{nombre}/?{campo}={valor}")
+        r.raise_for_status()
         return r.json().get('results')[0].get('id')
-    except IndexError:
+    except (IndexError, KeyError, JSONDecodeError):
         logger.exception(f"No se encontro el ID de {valor}")
         raise
     except RequestException:
-        logger.exception("Error al obtener objeto: ")
+        logger.exception(f"Error al obtener objeto")
         raise
 
 
@@ -27,25 +29,33 @@ def create_wrf_object(api_base_url, token, nombre, payload: dict):
     headers_base = {'Authorization': f'Token {token}'}
     try:
         r = requests.post(f"{api_base_url}/{nombre}/", json=payload, headers=headers_base)
+        r.raise_for_status()
+        wrf_id = r.json().get('id')
+        return wrf_id
     except RequestException:
         logger.exception(f"Error al crear objeto - nombre={nombre},payload={payload}")
         raise
-    if r.status_code != API_RESPONSES['post_ok']:
-        logger.error(f"bad_request->s_code = {r.status_code} - nombre={nombre},payload={payload}")
-        raise RequestException  # TODO: Revisar que sucede si ya existe el objeto
-    else:
-        wrf_id = r.json().get('id')
-        return wrf_id
+    except (JSONDecodeError, KeyError):
+        logger.exception("Error al decodificar las respuesta")
+        raise
 
 
 def ingest_csv_to_db(cuencas_dict: dict):
     logger.info("Iniciando ingestor de datos")
     timestamp = datetime.datetime.strftime(cuencas_dict['meta']['timestamp'], '%Y-%m-%d %H:%M')
     for api_base_url, meta in API_BASE_URL_DICT.items():
-        parametrizacion_id = get_wrf_api_object_id(api_base_url, 'parametrizacion', cuencas_dict['meta']['param'],
-                                                   campo='search')
+        try:
+            parametrizacion_id = get_wrf_api_object_id(api_base_url, 'parametrizacion', cuencas_dict['meta']['param'],
+                                                       campo='search')
+        except Exception:
+            logger.warning(f"No se pudo realizar la ingestion para {api_base_url}")
+            continue
         for prod in cuencas_dict['csv'].keys():
-            producto_cuenca_id = get_wrf_api_object_id(api_base_url, 'producto-cuencas', prod, campo='nombre')
+            try:
+                producto_cuenca_id = get_wrf_api_object_id(api_base_url, 'producto-cuencas', prod, campo='nombre')
+            except Exception:
+                logger.warning(f"No se pudo realizar la ingestion para {api_base_url}")
+                continue
             path = cuencas_dict['csv'][prod]['path']
             acumulacion = cuencas_dict['csv'][prod]['acumulacion']
             payload = {
@@ -57,6 +67,6 @@ def ingest_csv_to_db(cuencas_dict: dict):
             }
             try:
                 create_wrf_object(api_base_url, meta['token'], nombre='cuencas', payload=payload)
-            except RequestException:
+            except Exception:
                 pass
             time.sleep(0.3)
