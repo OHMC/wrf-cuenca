691774ebcf5ac8d173c2ed20e4b68bb0b36f9651
agumartina <amartina@unc.edu.ar>
2021-01-21 11:20:48 -0300

    updated path

--- src/api_ingest_csv.py
+++ src/api_ingest_csv.py
@@ -7,7 +7,7 @@ from wrf_api.cuencas_api_ingest import ingest_csv_to_db
 logger = get_logger_from_config_file(logger_name=INGESTOR_LOGGER_NAME)
 
 
-def load_wrf_api_dicts() -> dict:
+def load_wrf_api_dicts():
     """
     Carga los diccionarios generados en tablas_datos con los datos para ingestar en ld DB
 
@@ -18,7 +18,7 @@ def load_wrf_api_dicts() -> dict:
     """
     try:
         with open(CUENCAS_API_PICKLE_PATH, mode='rb') as w_api_f:
-            cuencas_api_dict: dict = pickle.load(w_api_f)
+            cuencas_api_dict = pickle.load(w_api_f)
     except (OSError, pickle.PickleError):
         logger.exception("Error al cargar diccionarios")
         raise
